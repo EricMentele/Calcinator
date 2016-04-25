d4c8934fc7498cf4a46b29b3371911e1eb80d619
EricMentele <diviningsage@gmail.com>
2016-04-25 00:03:16 -0700

    Initial Commit

---
+++ CalcinatorTests/CalcinatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalcinatorTests.swift
+//  CalcinatorTests
+//
+//  Created by Eric Mentele on 4/25/16.
+//  Copyright © 2016 Eric Mentele. All rights reserved.
+//
+
+import XCTest
+@testable import Calcinator
+
+class CalcinatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
