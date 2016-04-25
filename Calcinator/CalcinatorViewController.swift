2ef21740c022d7b31f480d412b49035f249ecbfe
EricMentele <diviningsage@gmail.com>
2016-04-25 00:37:42 -0700

    initial commit. Classes set up.

---
+++ Calcinator/CalcinatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalcinatorViewController.swift
+//  Calcinator
+//
+//  Created by Eric Mentele on 4/25/16.
+//  Copyright © 2016 Eric Mentele. All rights reserved.
+//
+
+import UIKit
+
+class CalcinatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Calcinator/Calculator.swift
@@ -0,0 +1,14 @@
+//
+//  Calculator.swift
+//  Calcinator
+//
+//  Created by Eric Mentele on 4/25/16.
+//  Copyright © 2016 Eric Mentele. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    var operand = 0
+    
+}
